abbbd111863f3c10365d4f7a6cee26f383e164c4
Ali <59686248+Alyster27@users.noreply.github.com>
2021-12-21 16:21:24 +0100

    add CompilerOptions in tsconfig.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as data from './Data/data.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-read-data-json';
+
+  ngOnInit() {
+      console.log(data);
+  }
+
 }
